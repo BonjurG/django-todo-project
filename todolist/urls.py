92f203413236863205bcb5fdf11b4d8afedf744a
BonjurG <hmgrem@yandex.ru>
2022-05-29 21:24:16 +0500

    make func of changes our tasks

--- todolist/urls.py
+++ todolist/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodo, name='currenttodo'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
 
 ]
