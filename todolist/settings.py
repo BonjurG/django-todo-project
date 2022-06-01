9c3dceae9e99234c7e74cc60aea01f8fd08a54cc
BonjurG <hmgrem@yandex.ru>
2022-06-01 15:39:05 +0500

    make online

--- todolist/settings.py
+++ todolist/settings.py
@@ -121,6 +121,11 @@ STATIC_URL = 'static/'
 LOGIN_URL = '/login'
 STATIC_ROOT = BASE_DIR / 'static'
 
+try:
+    from .localsett import *
+except ImportError:
+    print('Looks like no local file. You must be on production')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
